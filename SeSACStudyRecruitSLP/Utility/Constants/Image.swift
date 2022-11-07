a05e5b14e5a3ef6be7354bdbb0628bf6e285fdbd
minhye9731 <jenney62639731@gmail.com>
2022-11-07 16:49:29 +0900

    Feat: 아이콘 추가

---
+++ SeSACStudyRecruitSLP/Utility/Constants/Image.swift
@@ -0,0 +1,16 @@
+//
+//  Image.swift
+//  SeSACStudyRecruitSLP
+//
+//  Created by 강민혜 on 11/7/22.
+//
+
+extension Constants {
+    
+    enum ImageName: String {
+        
+        case magnifyingGlass = "magnifyingglass"
+
+    }
+    
+}
