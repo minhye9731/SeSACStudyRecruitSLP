acab6cfea82adda5bd3804d05d48b027e29cacc5
minhye9731 <jenney62639731@gmail.com>
2022-11-07 17:15:54 +0900

    Feat: Asset내 Icon 추가

---
+++ SeSACStudyRecruitSLP/Scene/WalkThrough/FirstViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FirstViewController.swift
+//  SeSACStudyRecruitSLP
+//
+//  Created by 강민혜 on 11/7/22.
+//
+
+import UIKit
+
+class FirstViewController:  {
+    
+    
+    
+    
+    
+}
