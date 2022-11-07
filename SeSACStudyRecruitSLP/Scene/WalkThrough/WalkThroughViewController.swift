a7b374b762858a90ab29f354ba00aa181ef772e3
minhye9731 <jenney62639731@gmail.com>
2022-11-07 16:49:56 +0900

    Feat: 온보딩 뷰컨 추가

---
+++ SeSACStudyRecruitSLP/Scene/WalkThrough/WalkThroughViewController.swift
@@ -0,0 +1,31 @@
+//
+//  WalkThroughViewController.swift
+//  SeSACStudyRecruitSLP
+//
+//  Created by 강민혜 on 11/7/22.
+//
+
+import UIKit
+
+class WalkThroughViewController: UIPageViewController {
+    
+    var pageViewController: [UIViewController] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    func createPageViewController() {
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+}
