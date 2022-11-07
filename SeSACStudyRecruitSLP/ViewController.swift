09b6d318817e09ef48c0191aff2930d2bf19832b
minhye9731 <jenney62639731@gmail.com>
2022-11-07 14:59:29 +0900

    Initial Commit

---
+++ SeSACStudyRecruitSLP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSACStudyRecruitSLP
+//
+//  Created by 강민혜 on 11/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
