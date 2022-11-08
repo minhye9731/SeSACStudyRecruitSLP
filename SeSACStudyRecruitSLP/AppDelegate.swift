1b7cde57028d5c42944b1d9129baf219223cf6f1
minhye9731 <jenney62639731@gmail.com>
2022-11-08 16:16:12 +0900

    Feat: AppDelegate에서 라이트모드만 대응 설정

--- SeSACStudyRecruitSLP/AppDelegate.swift
+++ SeSACStudyRecruitSLP/AppDelegate.swift
@@ -13,7 +13,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        self.window?.overrideUserInterfaceStyle = .light
+        
+        
+        
+        
         return true
     }
 
