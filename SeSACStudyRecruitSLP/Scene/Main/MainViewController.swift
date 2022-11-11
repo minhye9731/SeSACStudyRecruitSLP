2baae63e33d668d7e78b0a4be798d8bb0159bb90
minhye9731 <jenney62639731@gmail.com>
2022-11-12 00:52:56 +0900

    Feat: Main 화면 생성

---
+++ SeSACStudyRecruitSLP/Scene/Main/MainViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MainViewController.swift
+//  SeSACStudyRecruitSLP
+//
+//  Created by 강민혜 on 11/11/22.
+//
+
+import UIKit
+
+final class MainViewController: BaseViewController {
+    
+    // MARK: - property
+    
+    // MARK: - Lifecycle
+    override func loadView()  {
+        super.loadView()
+    }
+    
+    // MARK: - functions
+    
+    override func configure() {
+        super.configure()
+        view.backgroundColor = .systemMint
+    }
+
+}
