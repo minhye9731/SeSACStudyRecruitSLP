07f8ff1ac4bcdef8a8bb45846e679561c0f8bca4
minhye9731 <jenney62639731@gmail.com>
2022-11-13 12:44:32 +0900

    Feat: 로그인 통신 에러 정의

--- SeSACStudyRecruitSLP/Network/LoginError.swift
+++ SeSACStudyRecruitSLP/Network/LoginError.swift
@@ -7,4 +7,28 @@
 
 import Foundation
 
+enum LoginError: Int, Error {
+    case fbTokenError = 401
+    case unknownUser = 406
+    case serverError = 500
+    case clientError = 501
+}
+
+extension LoginError: LocalizedError {
+    
+    var errorDescription: String? {
+        switch self {
+        case .fbTokenError:
+            return "토큰이 만료되었습니다. 전화번호 인증을 다시 해주세요."
+        case .unknownUser:
+            return "미가입된 사용자입니다. 회원가입을 진행해주세요."
+        case .serverError:
+            return "서버에러가 발생했습니다. 로그인을 다시 시도해주세요."
+        case .clientError:
+            return "입력정보가 적절하지 않습니다. 다시 시도해주세요."
+        }
+    }
+    
+}
+
 
