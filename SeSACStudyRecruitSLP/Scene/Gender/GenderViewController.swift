0228718b8fdb5ba68eb7d3ad60d1da2a09571860
minhye9731 <jenney62639731@gmail.com>
2022-11-14 00:48:56 +0900

    Feat: 회원가입 서버통신 코드 추가

--- SeSACStudyRecruitSLP/Scene/Gender/GenderViewController.swift
+++ SeSACStudyRecruitSLP/Scene/Gender/GenderViewController.swift
@@ -63,11 +63,70 @@ final class GenderViewController: BaseViewController {
         if !maleSelected && !femaleSelected {
             self.mainView.makeToast("성별을 선택해 주세요.", duration: 1.0, position: .center)
         } else {
-            let value = (maleSelected && !femaleSelected) ? 1 : 0
+            let value = (maleSelected && !femaleSelected) ? "1" : "0"
             print("성별선택 저장값 = \(value)")
             UserDefaults.standard.set(value, forKey: "gender")
-            changeRootVC(vc: MainViewController())
+            trySignup()
         }
     }
+    
+    func trySignup() {
+        // 회원가입 정보 property wrapper 정리하자
+        let api = APIRouter.signup(
+            phoneNumber: UserDefaults.standard.string(forKey: "phoneNum")!,
+            FCMtoken: UserDefaults.standard.string(forKey: "fcmToken")!,
+            nick: UserDefaults.standard.string(forKey: "nickName")!,
+            birth: UserDefaults.standard.string(forKey: "realAge")!,
+            email: UserDefaults.standard.string(forKey: "email")!,
+            gender: UserDefaults.standard.string(forKey: "gender")!
+        )
+        
+        Network.share.requestSignup(router: api) { [weak self] response in
+            
+            switch response {
+            case .success(let success):
+                print("===회원가입 성공! (홈 화면으로 전환)====")
+                self?.mainView.makeToast("회원가입이 완료되었습니다.", duration: 0.5, position: .center)
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                    let vc = MainViewController()
+                    self?.changeRootVC(vc: vc)
+                }
+                
+            case .failure(let error):
+                
+                let code = (error as NSError).code
+                print("failure // code = \(code)")
+                
+                guard let errorCode = SignupError(rawValue: code) else { return }
+                print("failure // errorCode = \(errorCode)")
+                
+                switch errorCode {
+                case .existUser:
+                    self?.mainView.makeToast(errorCode.errorDescription, duration: 1.0, position: .center)
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                        let vc = PhoneNumberViewController()
+                        self?.changeRootVC(vc: vc)
+                    }
+                case .invalidNickname:
+                    self?.mainView.makeToast(errorCode.errorDescription, duration: 1.0, position: .center)
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                        let vc = NickNameViewController()
+                        self?.changeRootVC(vc: vc)
+                    }
+                case .fbTokenError:
+                    self?.mainView.makeToast(errorCode.errorDescription, duration: 1.0, position: .center) { _ in
+                        // 토큰 갱신 후 재요청
+                    }
+                    
+                default :
+                    self?.mainView.makeToast(errorCode.errorDescription, duration: 1.0, position: .center)
+                }
+            }
+        }
+    }
+    
+    
+    
     
 }
