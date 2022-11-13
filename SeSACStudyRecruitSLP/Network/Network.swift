30e875f0eb3424a93d8cbbbbf77674b4e0b484ed
minhye9731 <jenney62639731@gmail.com>
2022-11-13 13:32:31 +0900

    Feat: 네트워크 클래스 생성

--- SeSACStudyRecruitSLP/Network/Network.swift
+++ SeSACStudyRecruitSLP/Network/Network.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+import Alamofire
+import RxSwift
+
+final class Network {
+    
+    static let share = Network()
+    
+    private init() { }
+    
+    // login(get)
+    func requestLogin<T: Codable>(type: T.Type = T.self, router: APIRouter, completion: @escaping (Result<T, Error>) -> Void) {
+        
+        AF.request(router).validate().responseDecodable(of: T.self) { response in
+            switch response.result {
+            case .success(let data):
+                completion(.success(data))
+            case .failure(_):
+                guard let statusCode = response.response?.statusCode else { return }
+                guard let error = LoginError(rawValue: statusCode) else { return }
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    // signup(post)
+    func requestSignup(router: APIRouter, completion: @escaping (Result<String, Error>) -> Void) {
+        
+        AF.request(router).responseString { response in
+            switch response.result {
+            case .success(let data):
+                completion(.success(data))
+            case .failure(_):
+                guard let statusCode = response.response?.statusCode else { return }
+                guard let error = SignupError(rawValue: statusCode) else { return }
+                completion(.failure(error))
+            }
+        }
+    }
+    
+}
