5ffe59686c65644fe5208fa22def29629575f5dd
minhye9731 <jenney62639731@gmail.com>
2022-11-16 19:27:13 +0900

    Feat: 메인화면 UI 구성

--- SeSACStudyRecruitSLP/Scene/MainSearch/MainView.swift
+++ SeSACStudyRecruitSLP/Scene/MainSearch/MainView.swift
@@ -10,27 +10,87 @@ import MapKit
 
 final class MainView: BaseView {
     
+    // MARK: - property
     let mapView: MKMapView = {
         let map = MKMapView()
-        
         return map
     }()
     
+    let allbtn: UIButton = {
+        let button = UIButton.filterButton(title: "전체", textcolor: .white, bgcolor: ColorPalette.green)
+        button.clipsToBounds = true
+        button.layer.cornerRadius = 8
+        button.layer.maskedCorners = CACornerMask(arrayLiteral: .layerMinXMinYCorner, .layerMaxXMinYCorner)
+        return button
+    }()
+    let manbtn = UIButton.filterButton(title: "남자", textcolor: .black, bgcolor: .white)
+    let womanbtn: UIButton = {
+        let button = UIButton.filterButton(title: "여자", textcolor: .black, bgcolor: .white)
+        button.clipsToBounds = true
+        button.layer.cornerRadius = 8
+        button.layer.maskedCorners = CACornerMask(arrayLiteral: .layerMinXMaxYCorner, .layerMaxXMaxYCorner)
+        return button
+    }()
+    let locationbtn: UIButton = {
+        let button = UIButton()
+        button.configuration = UIButton.iconButton(image: Constants.ImageName.place.rawValue, fgcolor: .black, bgcolor: .white)
+        button.layer.shadowColor = UIColor.gray.cgColor
+        button.layer.shadowOpacity = 1.0
+        button.layer.shadowOffset = .zero
+        button.layer.shadowRadius = 2
+        return button
+    }()
     
-    
+    let floatingButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: Constants.ImageName.defaultState.rawValue), for: .normal)
+        return button
+    }()
+
+    // MARK: - functions
     override func configureUI() {
         super.configureUI()
         
-        self.addSubview(mapView)
+        [mapView, floatingButton, allbtn, manbtn, womanbtn, locationbtn].forEach {
+            self.addSubview($0)
+        }
     }
     
     override func setConstraints() {
         super.setConstraints()
         
+        let spacing = 16
+        
         mapView.snp.makeConstraints { make in
             make.edges.equalTo(self)
         }
+        
+        allbtn.snp.makeConstraints { make in
+            make.leading.top.equalTo(self.safeAreaLayoutGuide).offset(spacing)
+            make.width.height.equalTo(spacing * 3)
+        }
+        manbtn.snp.makeConstraints { make in
+            make.leading.equalTo(self.safeAreaLayoutGuide).offset(spacing)
+            make.top.equalTo(allbtn.snp.bottom)
+            make.width.height.equalTo(spacing * 3)
+        }
+        womanbtn.snp.makeConstraints { make in
+            make.leading.equalTo(self.safeAreaLayoutGuide).offset(spacing)
+            make.top.equalTo(manbtn.snp.bottom)
+            make.width.height.equalTo(spacing * 3)
+        }
+        
+        locationbtn.snp.makeConstraints { make in
+            make.leading.equalTo(self.safeAreaLayoutGuide).offset(spacing)
+            make.top.equalTo(womanbtn.snp.bottom).offset(spacing)
+            make.width.height.equalTo(spacing * 3)
+        }
+        
+        floatingButton.snp.makeConstraints { make in
+            make.width.height.equalTo(spacing * 4)
+            make.trailing.equalTo(self.safeAreaLayoutGuide).offset(-spacing)
+            make.bottom.equalTo(self.safeAreaLayoutGuide).offset(-spacing)
+        }
     }
-    
     
 }
