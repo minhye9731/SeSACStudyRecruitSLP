4c77937a360ec6eec22f2596e9621fb8f763d1b6
minhye9731 <jenney62639731@gmail.com>
2022-11-18 04:42:27 +0900

    Feat: 정보관리 화면 viewmodel 파일 생성

---
+++ SeSACStudyRecruitSLP/Scene/MyInfo/InfoManage/InfoManageViewModel.swift
@@ -0,0 +1,27 @@
+//
+//  InfoManageViewModel.swift
+//  SeSACStudyRecruitSLP
+//
+//  Created by 강민혜 on 11/18/22.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+final class InfoManageViewModel: CommonViewModel {
+    
+    // MARK: - property
+    struct Input {
+    }
+    
+    struct Output {
+
+    }
+    
+    // MARK: - functions
+    func transform(input: Input) -> Output {
+
+        return Output()
+    }
+}
