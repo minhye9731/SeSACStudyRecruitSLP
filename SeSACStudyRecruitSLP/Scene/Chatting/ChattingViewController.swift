f194919be7d3a0d9ebd01035c885b53eb15fad0b
minhye9731 <jenney62639731@gmail.com>
2022-11-20 01:03:48 +0900

    Feat: 사용자 상태별 화면전환 코드 추가

---
+++ SeSACStudyRecruitSLP/Scene/Chatting/ChattingViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ChattingViewController.swift
+//  SeSACStudyRecruitSLP
+//
+//  Created by 강민혜 on 11/20/22.
+//
+
+import UIKit
+
+final class ChattingViewController: BaseViewController {
+    
+    
+    override func configure() {
+        super.configure()
+        
+        view.backgroundColor = .systemMint
+    }
+    
+    override func setConstraints() {
+        super.setConstraints()
+        
+    }
+    
+}
--- SeSACStudyRecruitSLP/Scene/MainSearch/Main/MainViewController.swift
+++ SeSACStudyRecruitSLP/Scene/MainSearch/Main/MainViewController.swift
@@ -12,6 +12,7 @@ final class MainViewController: BaseViewController {
     // MARK: - property
     let userData: LoginResponse = LoginResponse(id: "", v: 0, uid: "", phoneNumber: "", email: "", fcMtoken: "", nick: "", birth: "", gender: 0, study: "", comment: [], reputation: [], sesac: 0, sesacCollection: [], background: 0, backgroundCollection: [], purchaseToken: [], transactionID: [], reviewedBefore: [], reportedNum: 0, reportedUser: [], dodgepenalty: 0, dodgeNum: 0, ageMin: 0, ageMax: 0, searchable: 0, createdAt: "")
     let mainView = MainView()
+    var matchingMode: MatchingMode = .normal
 
     // MARK: - Lifecycle
     override func loadView()  {
@@ -29,8 +30,16 @@ final class MainViewController: BaseViewController {
 
     @objc func floatingButtonTapped() {
         
-        
-        
-        
+        switch matchingMode {
+        case .normal:
+            let vc = SearchViewController()
+            transition(vc, transitionStyle: .push)
+        case .standby:
+            let vc = SearchResultViewController()
+            transition(vc, transitionStyle: .push)
+        case .matching:
+            let vc = ChattingViewController()
+            transition(vc, transitionStyle: .push)
+        }
     }
 }
