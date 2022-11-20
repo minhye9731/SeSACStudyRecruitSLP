377344cbe218d1d130da2ce08313f99bafa8c376
minhye9731 <jenney62639731@gmail.com>
2022-11-20 18:51:05 +0900

    Feat: 위치권한 없을 경우 지도 중앙값 추가

--- SeSACStudyRecruitSLP/Scene/MainSearch/Main/MainViewController.swift
+++ SeSACStudyRecruitSLP/Scene/MainSearch/Main/MainViewController.swift
@@ -12,10 +12,14 @@ import CoreLocation
 final class MainViewController: BaseViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     
     // MARK: - property
-    let userData: LoginResponse = LoginResponse(id: "", v: 0, uid: "", phoneNumber: "", email: "", fcMtoken: "", nick: "", birth: "", gender: 0, study: "", comment: [], reputation: [], sesac: 0, sesacCollection: [], background: 0, backgroundCollection: [], purchaseToken: [], transactionID: [], reviewedBefore: [], reportedNum: 0, reportedUser: [], dodgepenalty: 0, dodgeNum: 0, ageMin: 0, ageMax: 0, searchable: 0, createdAt: "")
+//    let userData: LoginResponse = LoginResponse(id: "", v: 0, uid: "", phoneNumber: "", email: "", fcMtoken: "", nick: "", birth: "", gender: 0, study: "", comment: [], reputation: [], sesac: 0, sesacCollection: [], background: 0, backgroundCollection: [], purchaseToken: [], transactionID: [], reviewedBefore: [], reportedNum: 0, reportedUser: [], dodgepenalty: 0, dodgeNum: 0, ageMin: 0, ageMax: 0, searchable: 0, createdAt: "")
     
     let mainView = MainView()
+    let locationManager = CLLocationManager()
     var matchingMode: MatchingMode = .normal
+    
+    
+    // 위치권한 없을경우 대비용
     let campusLocation = CLLocationCoordinate2D(latitude: 37.517819364682694, longitude: 126.88647317074734)
 
     // MARK: - Lifecycle
@@ -23,7 +27,19 @@ final class MainViewController: BaseViewController, MKMapViewDelegate, CLLocatio
         super.loadView()
         self.view = mainView
         
-        mainView.floatingButton.addTarget(self, action: #selector(floatingButtonTapped), for: .touchUpInside)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // 1) (API) 사용자 현재 상태를 확인하고, 플로팅 버튼을 설정함
+        
+        
+        
+        // 2) 사용자 현재 위치를 확인하고, 지도의 중심을 설정함
+        // 위치 권한이 거부된 상태라면, 영등포캠퍼스를 기준으로 설정
+        
+        // 3) (API) 사용자가 지도에서 설정한 위치를 보내고, 응답값으로 타새싹들 지도에 표기
     }
     
     // MARK: - functions
@@ -32,8 +48,10 @@ final class MainViewController: BaseViewController, MKMapViewDelegate, CLLocatio
         
         mainView.mapView.delegate = self
         
-        // 현재는 캠퍼스로 설정하지만, 실시간으로 내위치 업데이트가 필요함
-        setCenterRegionAnnotation(center: campusLocation)
+        setCenterRegion(center: campusLocation) // 현재는 캠퍼스로 설정하지만, 실시간으로 내위치 업데이트가 필요함
+        setCenterPinFixed() // 내위치 핀은 항상 지도 중앙에 고정
+        
+        mainView.floatingButton.addTarget(self, action: #selector(floatingButtonTapped), for: .touchUpInside)
     }
 
  
@@ -67,14 +85,18 @@ extension MainViewController {
 extension MainViewController {
 
     // 맵뷰 중심잡기 & 어노테이션 추가
-    func setCenterRegionAnnotation(center: CLLocationCoordinate2D) {
+    func setCenterRegion(center: CLLocationCoordinate2D) {
         // 중심잡기
         let region = MKCoordinateRegion(center: center, latitudinalMeters: 700, longitudinalMeters: 700)
         mainView.mapView.setRegion(region, animated: true)
-        
-        // pin 표기
+    }
+    
+    func setCenterPinFixed() {
+        let coordinate = mainView.mapView.centerCoordinate
+        let fixedCenter = CLLocationCoordinate2D(latitude: coordinate.latitude, longitude: coordinate.longitude)
+
         let annotation = MKPointAnnotation()
-        annotation.coordinate = center
+        annotation.coordinate = campusLocation // fixedCenter
         mainView.mapView.addAnnotation(annotation)
     }
     
