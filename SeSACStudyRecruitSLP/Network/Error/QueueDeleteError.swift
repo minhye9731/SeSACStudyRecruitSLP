376b77c502398ff1d3996de4eddeb4c71ce5b56e
minhye9731 <jenney62639731@gmail.com>
2022-12-03 23:46:10 +0900

    Feat: Add Error case

---
+++ SeSACStudyRecruitSLP/Network/Error/QueueDeleteError.swift
@@ -0,0 +1,37 @@
+//
+//  QueueDeleteError.swift
+//  SeSACStudyRecruitSLP
+//
+//  Created by 강민혜 on 12/2/22.
+//
+
+import Foundation
+
+enum QueueDeleteError: Int, Error {
+    case success = 200
+    case alreayMatched = 201
+    case fbTokenError = 401
+    case unknownUser = 406
+    case serverError = 500
+    case clientError = 501
+}
+
+extension QueueDeleteError: LocalizedError {
+    
+    var errorDescription: String {
+        switch self {
+        case .success:
+            return "스터디 찾기 중단 성공"
+        case .alreayMatched:
+            return "누군가와 스터디를 함께하기로 약속하셨어요!"
+        case .fbTokenError:
+            return "토큰이 만료되었습니다. 전화번호 인증을 다시 해주세요."
+        case .unknownUser:
+            return "미가입된 사용자입니다. 회원가입을 진행해주세요."
+        case .serverError:
+            return "서버에러가 발생했습니다. 로그인을 다시 시도해주세요."
+        case .clientError:
+            return "입력정보가 적절하지 않습니다. 다시 시도해주세요."
+        }
+    }
+}
---
+++ SeSACStudyRecruitSLP/Network/Error/SearchError.swift
@@ -0,0 +1,34 @@
+//
+//  SearchError.swift
+//  SeSACStudyRecruitSLP
+//
+//  Created by 강민혜 on 12/2/22.
+//
+
+import Foundation
+
+enum SearchError: Int, Error {
+    case success = 200
+    case fbTokenError = 401
+    case unknownUser = 406
+    case serverError = 500
+    case clientError = 501
+}
+
+extension SearchError: LocalizedError {
+    
+    var errorDescription: String? {
+        switch self {
+        case .success:
+            return "search 성공"
+        case .fbTokenError:
+            return "토큰에러가 발생했습니다. 다시 시도해주세요."
+        case .unknownUser:
+            return "미가입 사용자입니다. 회원가입을 진행해주세요."
+        case .serverError:
+            return "서버에러가 발생했습니다. 잠시 후 다시 시도해주세요."
+        case .clientError:
+            return "입력정보가 적절하지 않습니다. 다시 시도해주세요."
+        }
+    }
+}
