0ca6d14334c687f1cd28b16743e2568e8bbf001c
minhye9731 <jenney62639731@gmail.com>
2022-12-11 01:25:14 +0900

    Feat: Add review textview placeholder word at enum case

---
+++ SeSACStudyRecruitSLP/Utility/Constants/Word.swift
@@ -0,0 +1,15 @@
+//
+//  Word.swift
+//  SeSACStudyRecruitSLP
+//
+//  Created by 강민혜 on 12/11/22.
+//
+
+import Foundation
+
+extension Constants {
+    
+    enum Word: String {
+        case reviewPlaceholder = "자세한 피드백은 다른 새싹들에게 도움이 됩니다 (500자 이내 작성)"
+    }
+}
