6df38cbbb23ac02f454ac566b993e405b7c2d124
minhye9731 <jenney62639731@gmail.com>
2022-12-15 23:54:41 +0900

    Refactor: Change the type of priceButton from UIButton to PriceButton at ShopBackgroundCollectionViewCell

--- SeSACStudyRecruitSLP/Scene/Shop/ShopBackground/ShopBackgroundCollectionViewCell.swift
+++ SeSACStudyRecruitSLP/Scene/Shop/ShopBackground/ShopBackgroundCollectionViewCell.swift
@@ -24,8 +24,8 @@ final class ShopBackgroundCollectionViewCell: BaseCollectionViewCell {
         view.autoresizingMask = .flexibleHeight
         return view
     }()
-    let priceButton: UIButton = {
-        let btn = UIButton()
+    let priceButton: PriceButton = {
+        let btn = PriceButton()
         btn.contentEdgeInsets = UIEdgeInsets(top: 1.5, left: 6, bottom: 1.5, right: 6)
         btn.titleLabel?.font = CustomFonts.title5_M12()
         btn.layer.cornerRadius = 10 // check
