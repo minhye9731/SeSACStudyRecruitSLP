4f011a411ef3b8626b908996dcb49c06aba41094
minhye9731 <jenney62639731@gmail.com>
2022-12-18 23:50:56 +0900

    Feat: Create InAppProducts

---
+++ SeSACStudyRecruitSLP/Scene/Shop/InAppProducts.swift
@@ -0,0 +1,20 @@
+//
+//  InAppProducts.swift
+//  SeSACStudyRecruitSLP
+//
+//  Created by 강민혜 on 12/15/22.
+//
+
+//import Foundation
+//import StoreKit
+//
+//struct InAppProducts {
+//
+//    public static let productIdentifiers: Set<String> = [""]
+//
+//    public static let productArray = Array<SKProduct>()
+//
+//    var product: SKProduct?
+//
+//    public static let store = IAPHelper(productIds: InAppProducts.productIdentifiers)
+//}
