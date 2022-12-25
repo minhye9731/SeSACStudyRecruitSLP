1eb00bf2a4b90272d362c5cf7da20b863d475df0
minhye9731 <jenney62639731@gmail.com>
2022-12-26 02:37:10 +0900

    Feat: Create washable Review Struct to make ItemIdentifierType unique

---
+++ SeSACStudyRecruitSLP/Data/Review.swift
@@ -0,0 +1,13 @@
+//
+//  Review.swift
+//  SeSACStudyRecruitSLP
+//
+//  Created by 강민혜 on 12/26/22.
+//
+
+import Foundation
+
+struct Review: Hashable {
+    let identifier = UUID()
+    var comment: String
+}
